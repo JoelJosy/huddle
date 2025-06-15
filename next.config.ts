d17d665c2082dc51d0a837a6ce5b9ef6c290e3da
Joel Josy <joeljosy79@gmail.com>
2025-06-15 15:39:42 +0400

    update nextconfig for optimizations

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "ocvyaicrbpqrhmkgrlay.supabase.co",
-        pathname: "/storage/v1/object/public/my-images/**",
+        pathname: "/storage/v1/object/public/**",
       },
     ],
+    unoptimized: true,
   },
+  experimental: {
+    optimizePackageImports: [
+      "lucide-react",
+      "@tiptap/react",
+      "@tiptap/starter-kit",
+      "reactflow",
+    ],
+    serverComponentsHmrCache: true, // Cache Server Components during HMR
+  },
+  // Enable compression
+  compress: true,
+  // Enable static optimization
+  output: "standalone",
 };
 
-module.exports = nextConfig;
+export default nextConfig;
